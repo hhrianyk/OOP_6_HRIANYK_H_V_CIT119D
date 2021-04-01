76b85a8aedc6bea85beeff3fa0c8d0d07b0040aa
hhrianyk <71703458+hhrianyk@users.noreply.github.com>
2021-04-01 20:13:08 +0300

    Update Comment

--- src/ua/khpi/oop/Hrianyk13/Obshchak.java
+++ src/ua/khpi/oop/Hrianyk13/Obshchak.java
@@ -1,10 +1,10 @@
 package ua.khpi.oop.Hrianyk13;
-
+///клас для загального використанння
 public class Obshchak {
 	
-	public static ContainerList<PoliceFile> List=new ContainerList<PoliceFile>();
-	public static Boolean stop ;
-	public static int TimeOut=0; 
-	public static int Choice=0; 
+	public static ContainerList<PoliceFile> List=new ContainerList<PoliceFile>();///container
+	public static Boolean stop ; ///Flag status of interactive use of drinks (false - used; true- no)
+	public static int TimeOut=0; ///TimeOut: if TimeOut=0 operating time is not limited otherwise limited by value
+	public static int Choice=0; ///id process/thread
 
 }
