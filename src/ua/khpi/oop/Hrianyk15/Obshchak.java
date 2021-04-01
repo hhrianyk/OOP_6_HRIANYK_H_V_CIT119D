e73586461ce0f24553e4be9711597387e4be9b82
hhrianyk <71703458+hhrianyk@users.noreply.github.com>
2021-04-01 20:14:54 +0300

    Update Comment

--- src/ua/khpi/oop/Hrianyk15/Obshchak.java
+++ src/ua/khpi/oop/Hrianyk15/Obshchak.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 public class Obshchak {
 	
-	public static ArrayList List=new ArrayList<PoliceFile>();
-	public static Boolean stop ;
-	public static int TimeOut=0; 
-	public static int Choice=0; 
+	public static ContainerList<PoliceFile> List=new ContainerList<PoliceFile>();///container
+	public static Boolean stop ; ///Flag status of interactive use of drinks (false - used; true- no)
+	public static int TimeOut=0; ///TimeOut: if TimeOut=0 operating time is not limited otherwise limited by value
+	public static int Choice=0; ///id process/thread
 
 }
